3bddbdbec43c23de6e6580358d42f4ab4044efa6
Christopher Persaud <christopherp543@nycstudents.net>
2024-01-12 15:27:21 -0500

    b

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function shuffle(){
     let nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+    
     for (let i = 1;i < 17;i++){
         console.log(nums);
         const img = document.getElementById(`p${i}`);
-        const num = Math.floor(Math.random()*nums.length) + 1;
-        console.log(nums.length);
-        console.log(`num ${num}`);
+        const num = Math.floor(Math.random()*nums.length);
        img.src = `chicken${nums[num]}.png`;
         nums.splice(num,1);
     }
+    
+}
+
+function movePiece(){
+    
 }
 
 shuffle();
