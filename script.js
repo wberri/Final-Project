5f7c0683a527a7e24aa1964704b988e98dbc5975
wafa <wafab@nycstudents.net>
2024-01-22 15:15:09 -0500

    h

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let selectedPiece = null;
 let selectedDiv = null;
-
+let clicks = 0;
 
 function shuffle(){
     let nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
     
     for (let i = 1;i < 17;i++){
-        console.log(nums);
         const img = document.getElementById(`p${i}`);
         const num = Math.floor(Math.random()*nums.length);
        img.src = `chicken${nums[num]}.png`;
         nums.splice(num,1);
     }
+    document.getElementById('congratulations').textContent = "";
+    clicks = 0;
+    document.getElementById('clicks').textContent = `Clicks: ${clicks}`;
     
 }
 
@@ -31,19 +33,22 @@ function selectPiece(){
     for (let i = 1; i< 17; i++){
         document.getElementById(`p${i}`).addEventListener('click',placePiece);
     }
-    console.log(selectedPiece.id);
-
+   
 }
 
 function checkPuzzle(){
+    let sum = 0;
     for (let i = 1; i < 17; i++){
         let piece = document.getElementById(`p${i}`);
-        if (piece.src.text != `chicken${i}`){
-            return  false;
+        let nam = piece.src.substring(piece.src.indexOf("chicken"));
+        if (nam != `chicken${i}.png`){
+           
+            return false;
         }
     }
-    return true;
-}
+        return true;
+    }
+
 
 function placePiece(){
     if (this.id.substring(0,1) != "p" || this.id.substring(0,2) == "ps"){
@@ -59,17 +64,32 @@ function placePiece(){
     this.src = temp;
     selectedPiece = null;
     selectedDiv = null;
-    console.log(checkPuzzle());
+    clicks++;
+    document.getElementById('clicks').textContent = `Clicks: ${clicks}`;
     for (let i = 1; i< 17; i++){
         document.getElementById(`p${i}`).addEventListener('click',selectPiece);
     }
+    if (checkPuzzle()){
+        document.getElementById('congratulations').textContent = "Congratulations you solved the puzzle!!1";
+    }
 
+}
+
+function fixPuzzle(){
+    for (let i = 1; i < 17; i++){
+        const imag = document.getElementById(`p${i}`);
+        imag.src = `chicken${i}.png`;
+        clicks = 0;
+        document.getElementById('clicks').textContent = `Clicks: ${clicks}`;
+    }
+    document.getElementById('congratulations').textContent = "";
 }
 
 for (let i = 1; i< 17; i++){
     document.getElementById(`p${i}`).addEventListener('click',selectPiece);
 }
 
+document.getElementById('shuffle').addEventListener('click', shuffle);
+document.getElementById('restart').addEventListener('click', fixPuzzle);
 
 
-shuffle();
